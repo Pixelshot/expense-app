2ec95f73ea4f308f5a605173f6134f45d620b21e
Pixelshot <pixelshot8@gmail.com>
2022-12-29 18:30:16 +0800

    Database is connected to form but need to change it to <Form>. Server side validation is done.

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -1,6 +1,9 @@
+import { redirect } from '@remix-run/node';
 import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
+import { addExpense } from '~/data/expenses.server';
+import { validateExpenseInput } from '~/data/validation.server';
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -12,4 +15,19 @@ export default function AddExpensesPage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    // If data is returned instead of being redirected, the data can be accessed in our component
+    return error;
+  }
+
+  await addExpense(expenseData);
+  return redirect('/expenses');
 }
