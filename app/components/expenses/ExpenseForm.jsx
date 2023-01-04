6a90fd59c9c588a9faead73787efc23e6a776b72
Pixelshot <pixelshot8@gmail.com>
2023-01-04 18:07:07 +0800

    useTransition()/useNavigation() to let the user know what's going on with the submitted form

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -1,32 +1,27 @@
-import { Link, useActionData } from '@remix-run/react';
+import {
+  Form,
+  Link,
+  useActionData,
+  useTransition as useNavigation,
+} from '@remix-run/react';
+
+// useTransition() as useNavigation() because the name will be changed to useNavigation() in the near future
 
 // useActionData() and useLoaderData() can also be called in a component, not just in a route file.
 
 function ExpenseForm() {
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
   const validationErrors = useActionData();
+  const navigation = useNavigation();
 
-  // This code is just to show how to use useSubmit() hook
-  // For more info, see vid 63
+  // This useTransition()/useNavigation() hook can only work with <Form /> not <form />
 
-  // import { useSubmit} from '@remix-run/react'
-
-  // const submit = useSubmit()
-
-  // function submitHandler(event) {
-  //   event.preventDefault();
-  // perform your own validation
-  // ...
-  //   submit(event.target, {
-  //     action: 'expenses/add',
-  //     method: 'post'
-  //   })
-  // }
-
-  // This function can the be hooked to onSubmit() in our form
+  // Letting the user know what's going on with the submitted form
+  // this can be used at our button inside of <Form>
+  const isSubmitting = navigation.state !== 'idle';
 
   return (
-    <form method="post" className="form" id="expense-form">
+    <Form method="post" className="form" id="expense-form">
       <p>
         <label htmlFor="title">Expense Title</label>
         <input type="text" id="title" name="title" required maxLength={30} />
@@ -57,11 +52,33 @@ function ExpenseForm() {
         </ul>
       )}
       <div className="form-actions">
-        <button>Save Expense</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Saving...' : 'Save Expense'}
+        </button>
         <Link to="..">Cancel</Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
 export default ExpenseForm;
+
+// This code is just to show how to use useSubmit() hook
+// For more info, see vid 63
+
+// import { useSubmit} from '@remix-run/react'
+
+// insert this section and the function below inside of action()
+// const submit = useSubmit()
+
+// function submitHandler(event) {
+//   event.preventDefault();
+// perform your own validation
+// ...
+//   submit(event.target, {
+//     action: 'expenses/add',
+//     method: 'post'
+//   })
+// }
+
+// This function can the be hooked to onSubmit() in our form
