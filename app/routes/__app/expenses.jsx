3867981826bb7895ae67e1299fbb68ce76110758
Pixelshot <pixelshot8@gmail.com>
2023-01-08 12:42:31 +0800

    How Remix renders single page app both server-side and front-end

--- app/routes/__app/expenses.jsx
+++ app/routes/__app/expenses.jsx
@@ -7,6 +7,8 @@ import { FaPlus, FaDownload } from 'react-icons/fa';
 // All components of the same route has access to the routes useLoader() and useAction() data
 
 export default function ExpenseOutlet() {
+  // server-side codes(eg: console.log()) in functions outside of loader() can end up on the front-end. Therefore, it's best to keep it inside of loader() since they always only run on the server-side
+  // Similar concept applies to front-end codes only as well
   const expenses = useLoaderData();
   return (
     <>
@@ -29,6 +31,11 @@ export default function ExpenseOutlet() {
 }
 
 // in this case, async can be omitted from loader() because the function that we're calling(getExpenses()) is already an async function that is resolved within itself via the return keyword. vid 65
-export function loader() {
-  return getExpenses();
+export async function loader() {
+  const expenses = await getExpenses();
+  // Remix will automatically wrap any return raw data into a response
+  // return json(expenses); // This is returning raw data(json format). But because of the explanation above, the result will be the same as below:
+  // Don't forget to import {json} from @remix/node if you're using it
+  // For more of this and explanation on single page see vid #66
+  return expenses;
 }
